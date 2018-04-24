877c7f07ae9f49ba1ae6989a483b4b7a9164f740
Sakif Imtiaz <simtiaz@mbmortgage.com>
2018-04-24 16:36:50 -0400

    task 2: average answer ratings

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -3,5 +3,15 @@ class Candidate < ApplicationRecord
   has_many :positions, through: :interviews
   has_many :questions, through: :interviews
   has_many :answers
+  has_many :answer_ratings, through: :answers # this is really just for testing
+  has_many :valid_answer_ratings, 
+  	-> {where "rating > 0"},
+  	class_name: "AnswerRating",
+  	through: :answers,
+  	source: :answer_ratings
+
+  def average_answer_rating
+  	valid_answer_ratings.average(:rating)
+  end
 
 end
--- test/models/candidate_test.rb
+++ test/models/candidate_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class CandidateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    create_answers_and_ratings
+  end
+
+  test "candidates should have answers" do
+  	c = candidates(:fred_flintstone)
+  	assert c.answers.any?
+  end
+
+  test "candidates' answers should have ratings" do
+  	c = candidates(:fred_flintstone)
+  	assert c.answer_ratings.any?
+  end
+
+  test "candidates' answers should have at least 1 0-rating" do
+  	c = candidates(:fred_flintstone)
+  	assert c.answer_ratings.any? { |ar|  ar.rating == 0 }
+  end
+
+  test "the `average_answer_rating` should be the average of the nonzero answer ratings" do
+  	c = candidates(:fred_flintstone)
+  	ratings = c.answer_ratings.pluck(:rating).reject(&:negative?)
+  	assert (ratings.inject(&:+) / ratings.size.to_f), c.average_answer_rating
+  end
+
 end
